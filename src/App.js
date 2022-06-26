7467f53705a999752e7f010c477309e006aefd2a
Umurzakov0204 <tomthompson0222@gmail.com>
2022-06-26 15:27:30 +0500

    ant

--- src/App.js
+++ src/App.js
@@ -1,11 +1,76 @@
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import React, { useState } from 'react';
 import './App.css';
+import 'antd/dist/antd.css';
+import {NavLink} from "react-router-dom";
 
-function App() {
+const { Header, Sider, Content } = Layout;
+
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <div className="main">
-        <h2 className="main-header">React Crud Operations</h2>
-    </div>
+      <Layout>
+        <Sider style={{
+            overflow: 'auto',
+            height: '100vh'
+        }} trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              items={[
+                {
+                  key: '1',
+                  icon: <UserOutlined />,
+                  label: 'nav 1',
+                },
+                {
+                  key: '2',
+                  icon: <VideoCameraOutlined />,
+                  label: 'nav 2',
+                },
+                {
+                  key: '3',
+                  icon: <UploadOutlined />,
+                  label: 'nav 3',
+                },
+              ]}
+          />
+        </Sider>
+        <Layout className="site-layout">
+          <Header
+              className="site-layout-background"
+              style={{
+                padding: 0,
+              }}
+          >
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: () => setCollapsed(!collapsed),
+            })}
+          </Header>
+          <Content
+              className="site-layout-background"
+              style={{
+                margin: '24px 16px',
+                padding: 24,
+                minHeight: 280,
+              }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
   );
-}
+};
 
 export default App;
